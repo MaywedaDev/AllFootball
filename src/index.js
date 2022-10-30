e97432b559fea5b0c0f32448b440f55ee3d40ab1
MaywedaDev <enyoonuche1@gmail.com>
2022-10-30 21:13:53 +0100

    Update AllFootball

--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import { HashRouter } from 'react-router-dom';
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
@@ -18,9 +17,7 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 
 root.render(
     <React.StrictMode>
-      <HashRouter>
-        <App />
-      </HashRouter>
-  </React.StrictMode>
+      <App />
+    </React.StrictMode>
   );
 
